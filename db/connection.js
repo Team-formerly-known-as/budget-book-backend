8ca52fb34ac909391bc9a896b6c5dd1c9c674c65
Edgar Alfonso Lopez De Los Santos <edgaralfonsolopezdelossantos@Edgars-MacBook-Pro.local>
2021-11-23 15:30:32 -0800

    MongoDb deployment

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost/budget-book-backend")
+mongoose.connect("mongodb://localhost/budget-book-backend", {useNewUrlParser: true})
+let mongoURI = "";
+
+if (process.env.NODE_ENV === "production") {
+    mongoURI = process.env.DB_URL;
+  } else {
+    mongoURI = "mongodb://localhost/book-e";
+  }
 
 module.exports = mongoose;
