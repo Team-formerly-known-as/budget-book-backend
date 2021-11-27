f929e866c2a5e6304a17a42bfd734625223113e1
Edgar Alfonso Lopez De Los Santos <edgaralfonsolopezdelossantos@Edgars-MacBook-Pro.local>
2021-11-27 12:48:14 -0800

    Made new PUT request on userController

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -47,60 +47,51 @@ router.get("/:id", (req, res) => {
   );
 });
 
-//MAYBE WE USE????
-
-// router.get("/:id", (req, res) => {
- 
-//     User.findById(req.params.expenseId).populate('expenses').then((user) => {
-//       Expense.findByIdAndUpdate(req.params.userId, req.body, { new: true }).then(
-//         (expense) => {
-//           console.log("user", user);
-//           user.expenses.push(expense._id);
-         
-//           expense.save();
-//           console.log("user2", user);
-//           res.json({
-//             status: 200,
-//             user: user,
-//           });
-//         }
-//       );
-//     });
-//   });
-// router.post('/:new'),(req, res) => {
-//     User.create(req.body.user).then( user => {
-//         Expense.create(req.body.expense).then( expense => {
-//             console.log( req.body.user, req.body.expense)
-//             user.expenses.push(expense._id)
-//             expense.users.push(user._id)
-//             res.json(user)
-//         })
-//     })
-// }
-
 router.put("/:expenseId/:userId", (req, res) => {
  
-  Expense.findById(req.params.expenseId).then((expense) => {
-    console.log("expense", expense);
-    User.findByIdAndUpdate(req.params.userId, req.body, { new: true }).then(
-      (user) => {
-        console.log("user", user);
-        user.expenses.push(expense._id);
-       
-        expense.save();
-        console.log("user2", user);
-        res.json({
-          status: 200,
-          user: user,
-        });
-
-        //Expense.findById(req.params.expenseId).then( (expense) => {
-        //User.findByIdAndUpdate((req.params.userId), {$push: {expenses: expense.id}}, {new: true})
-        //.then((newUser) => { res.json(newUser)
-      }
-    );
+    Expense.findByIdAndUpdate(req.params.expenseId, req.body, { new: false}).then((expense) => {
+      console.log("expense", expense);
+      User.findByIdAndUpdate(req.params.userId, req.body, { new: true}).populate('expenses').then(
+        (user) => {
+          console.log("user", user);
+          user.expenses.push(expense);
+          
+         
+          expense.save();
+          console.log("user2", user);
+          res.json({
+            status: 200,
+            user: user,
+          });
+        }
+      );
+    });
   });
-});
+
+// router.put("/:expenseId/:userId", (req, res) => {
+ 
+//   Expense.findById(req.params.expenseId).then((expense) => {
+//     console.log("expense", expense);
+//     User.findByIdAndUpdate(req.params.userId, req.body, { new: true}).populate('expenses').then(
+//       (user) => {
+//         console.log("user", user);
+//         user.expenses.push(expense);
+        
+       
+//         expense.save();
+//         console.log("user2", user);
+//         res.json({
+//           status: 200,
+//           user: user,
+//         });
+
+//         //Expense.findById(req.params.expenseId).then( (expense) => {
+//         //User.findByIdAndUpdate((req.params.userId), {$push: {expenses: expense.id}}, {new: true})
+//         //.then((newUser) => { res.json(newUser)
+//       }
+//     );
+//   });
+// });
 
 // router.delete("/:expenseId/:ownerId", (req, res) => {
 
